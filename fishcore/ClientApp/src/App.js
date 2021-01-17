cf0f0587fb52cb234a86d3d2e08a72f88906a430
Christopher J Fraher <fraher@gmail.com>
2021-01-17 03:43:45 -0600

    Added React Flow; Created DevelopmentWindow

---
+++ fishcore/ClientApp/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import { Layout } from './components/Layout';
+import { Home } from './components/Home';
+import { FetchData } from './components/FetchData';
+import { Counter } from './components/Counter';
+
+import './custom.css'
+import DevelopmentWindow from './components/DevelopmentWindow';
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render () {
+    return (
+      <Layout>
+        <Route exact path='/' component={Home} />
+        <Route path='/counter' component={Counter} />
+        <Route path='/fetch-data' component={FetchData} />
+        <DevelopmentWindow />
+      </Layout>
+    );
+  }
+}
